efdfb570031cc7a36492e60dbd97744141789943
ErickLauren12 <ericklauren86@gmail.com>
2022-02-22 09:30:47 +0700

    Update 1

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            'category_name' => "ANALGESIK NARKOTIK"
+        ]);
+
+        DB::table('categories')->insert([
+            'category_name' => "ANALGESIK NON NARKOTIK"
+        ]);
+    }
+}
